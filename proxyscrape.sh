298124c39577ef612167d3b5bb106b245ffaf67e
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2025-01-06 15:23:16 -0300

    Create proxyscrape.sh

---
+++ proxyscrape.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# limpa a tela
+clear; 
+
+list=./proxyscrape.txt
+url="https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&protocol=http&proxy_format=protocolipport&format=text&timeout=1000"
+
+# puxa a lista pela api do proxiscrape
+if curl "$url" > "$list"; then
+  echo "lista salva em: $list"
+  else 
+    echo "erro"
+    exit 1;
+fi
+
+conferir_ping(){
+        while read -r link; do 
+        proxy=$(echo $link | awk -F '//' '{print $2}' | cut -d: -f1);
+        echo "";
+        echo "$link";
+        ping -c 1 $proxy; done < $list
+}
+
+if [[ "$1" == "-c" ]]; then 
+  conferir_ping
+fi
+    
